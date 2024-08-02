429958dde273f96befcac866dbb61ed7511f340f
Ulugbek-2008 <u20081219@gmail.com>
2024-08-03 00:28:03 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,128 @@
+
+
+
+
+// let numbers = [55, 43 , 19 , 56 , 76 , 24]
+// let juft = 0
+// let toq = 0
+
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] % 2 == 0){
+//         juft += numbers[i]
+//     }
+//     else{
+//         toq += numbers[i]
+//     }
+// }
+// console.log("Juftlari yig'indisi " + juft);
+// console.log("Toqlari yig'indisi " + toq);
+
+
+
+
+// let arr = [2,341,123,445,1231,545,12,5]
+// let son = prompt("Son kiriting") - 0
+
+// for(let i = 0 ; i < arr.length;i++){
+//     if(arr[i] == son){
+//         console.log(son + " arr da mavjud");
+//     }
+//     else if(arr[i] != son){
+//         console.log("Bunday son arrayda mavjud emas");
+//     }
+//  }
+
+
+
+
+// let Son = prompt("Son kiriting")
+// let sum = 0
+// for(let i = 1; i <= Son ; i++){
+//     sum += i
+// }
+// console.log(sum);
+
+
+
+
+
+// let Arr = [-28 , 56 , 4 , -59 , 45 , 12, 39]
+// let SON = prompt("Son kiriting") - 0
+// let Sum = 0
+// for(let i = 0; i <= SON; i++){
+//     Sum += Arr[i]
+// }
+// console.log("Arraydagi sonlar yigindisi " + Sum);
+
+
+
+
+
+// let totaldollor = prompt("Son kiriting")
+
+// let currencyDollar = 12500
+// let currencySom = currencyDollar * totaldollor
+
+// console.log(currencySom);
+
+
+
+// let raqam = prompt("Raqam kiriting")
+
+// if(raqam < 0){
+//     alert("Musbat son kiriting");
+// }
+// else if(raqam % 2 == 0){
+//     alert("Kvadrati Juft son")
+// }
+// else if (raqam % 2 != 0){
+//     alert("Kvadrati Toq son")
+// }
+
+
+
+// let n = prompt("Son kiriting")
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 !== 0) {
+//         console.log( i);
+//     }
+// }
+
+
+
+// let N = prompt("Son kiriting")
+// let n2 = 2
+// let a =[1 , 11 , 3 , 4 , 5 , 6 , 7]
+
+// if(N > 0){
+//     console.log("Musbat son kiriting");
+// }
+// else if(n2){
+//     console.log("2 soni mavjud");
+// }
+// else {
+//     console.log("2 soni mavjud emas");
+// }
+
+
+let names = ["Komiljon", "Yusufbek", "Islomjon", "Akbar"];
+let Name = prompt("Ism kiriting:");
+
+if (Name) {
+    console.log("Bunday ism mavjud.");
+}
+ else {
+    console.log("Bunday ism yo'q.");
+}
+
+
+
+// let result = prompt("Son kiriting")
+
+// for(let i = 1; i <=20; ){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
