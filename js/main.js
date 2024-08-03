95be643ce489b5ea761f49d7599582b3f6fc10fa
Ulugbek-2008 <u20081219@gmail.com>
2024-08-03 22:25:01 +0500

    Add js file

--- js/main.js
+++ js/main.js
@@ -105,15 +105,15 @@
 // }
 
 
-let names = ["Komiljon", "Yusufbek", "Islomjon", "Akbar"];
-let Name = prompt("Ism kiriting:");
+// let names = ["Komiljon", "Yusufbek", "Islomjon", "Akbar"];
+// let Name = prompt("Ism kiriting:");
 
-if (Name) {
-    console.log("Bunday ism mavjud.");
-}
- else {
-    console.log("Bunday ism yo'q.");
-}
+// if (Name) {
+//     console.log("Bunday ism mavjud.");
+// }
+//  else {
+//     console.log("Bunday ism yo'q.");
+// }
 
 
 
@@ -125,4 +125,145 @@ if (Name) {
 //     }
 // }
 
+// let userNumber = prompt("Son kiriting") - 0
+// let usersum = 0
+// for(let i = 1 ; i <= userNumber ; i++){
+//    if(i % 3 == 0){
+//     usersum += i
+//    }
+// }
+// console.log(usersum);
 
+
+
+// let evenSum = 0
+// let oddSum = 0
+
+// for(let i = 1; i <= 10;i++){
+//     if(i % 2 == 0){
+//         evenSum += i
+//     }
+//     else{
+//         oddSum += i
+//     }
+// }
+// let res = oddSum / evenSum
+// console.log(res);
+
+
+
+
+// let b = prompt("2-son kiritin") - 0
+// a < b
+// for(let i = a; i <= b; i++){
+//     console.log(i);
+    
+// }
+
+
+
+
+
+// let A = prompt("1-son kiritin") - 0 // 25
+// let firsNum = Math.floor(A / 10)
+// let secondNum = A % 10
+// let Res = (firsNum + secondNum) - firsNum
+// console.log(Res);
+
+
+
+// let array = ["salom", 23, true, null, undefined]
+// let isBool = 0
+// for(let i = 0 ; i < arr.length; i++){
+//     if(typeof array[i] == "boolean"){
+//         isBool++
+//     }
+// }
+// console.log(isBool > 0 ? "Bor" : "Yo'q");
+
+
+
+// let ARRAY = [-2, 4, -5, 6, -9, 11, 23 , 54, 23, -12]
+// let posCount = 0
+// let negCount = 0
+
+// for(let i = 0; i < arr.length;i++){
+//     if(arr[i] > 0){
+//         posCount++
+//     }
+//     else{
+//         negCount++
+//     }
+// }
+// console.log("Manfiylari => " + negCount);
+// console.log("Musbatlari => " + posCount);
+
+
+
+
+
+// for(let i = 20 ; i >= 1 ;i--){
+//     console.log(i);
+// }
+
+// let arr = [44, 33, 22, 3, 2, 1, 56, 3];
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
+
+
+
+
+
+// let number = (prompt("Biror son kiriting");
+
+// let sum = 0;
+
+// for (let i = 1; i <= number; i++) {
+//     sum += i;
+// }
+
+// console.log("Yig'indi " + sum);
+
+
+
+
+
+// let numbers = [-5, 2, -4, 99, 21, 2, -9, 52];
+
+// let negativeSum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] < 0) {
+//         negativeSum += numbers[i];
+//     }
+// }
+
+// console.log("Manfiy sonlar yig'indisi: " + negativeSum);
+
+
+
+
+// const numbers = [-5, 2, -4, 99, 21, 2, -9, 52];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log("Arrayning elementlari yig'indisi:", sum);
+
+
+
+
+// let array = [4, 2, 4, true, false, null];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (typeof array[i] === 'number') {
+//     sum += array[i];
+//   }
+// }
+
+// console.log("Array qiymatlari yig'indisi", sum); 
